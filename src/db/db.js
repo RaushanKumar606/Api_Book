13a7d5c8c865af0f616c53cc754e5305f38d9d20
Raushan kumar <roshankumarsingh964@gmail.com>
2024-10-26 00:05:12 +0530

    db

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-
 const connection = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Book_Api', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://127.0.0.1:27017/Book_Api', 
+      
+    );
     console.log("Connected to the database successfully!");
   } catch (error) {
     console.error("Database connection failed, retrying...");
