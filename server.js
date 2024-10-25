1e740fab9eb2df17e7476194bf11828e04551367
Raushan kumar <roshankumarsingh964@gmail.com>
2024-10-26 00:04:16 +0530

    add

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express =  require('express');
 require('dotenv').config()
 const {connection} = require('./src/db/db');
-
 const app =express();
+
+
+//  middleware 
+app.use('/api/auth')
+
+
+
+
+
 const PORT  = process.env.PORT || 3030;
 app.listen(PORT,()=>{
     console.log(`Server is on ${PORT}`);
